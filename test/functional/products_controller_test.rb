3063cb9c1607fbcce49f5a94baebbc485ebd0303
Heidar Bernhardsson <heidar@iseld.org>
2013-02-17 01:49:16 +0000

    Added functional tests for products controller + minor fixes
    Finished chapter 8.

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,29 +15,35 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select 'h1', 'Listing products'
+    assert_select '#product_list tr', 3
   end
 
   test "should get new" do
     get :new
     assert_response :success
+    assert_select 'h1', 'New product'
+    assert_select '#main form', 1
   end
 
   test "should create product" do
     assert_difference('Product.count') do
       post :create, :product => @update
     end
-
     assert_redirected_to product_path(assigns(:product))
   end
 
   test "should show product" do
     get :show, id: @product
     assert_response :success
+
   end
 
   test "should get edit" do
     get :edit, id: @product
     assert_response :success
+    assert_select 'h1', 'Editing product'
+    assert_select '#main form', 1
   end
 
   test "should update product" do
@@ -49,7 +55,6 @@ class ProductsControllerTest < ActionController::TestCase
     assert_difference('Product.count', -1) do
       delete :destroy, id: @product
     end
-
     assert_redirected_to products_path
   end
 end
