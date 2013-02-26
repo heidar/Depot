5604decdda39b870bb515f259ffd0306205fb74c
Heidar Bernhardsson <heidar@iseld.org>
2013-02-26 22:09:57 +0000

    Added support for multiple line items that are the same item in a cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :image_url, :price, :title
   default_scope :order => 'title'
   has_many :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
